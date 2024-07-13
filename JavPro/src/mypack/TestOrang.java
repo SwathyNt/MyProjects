c6af0c3fce8b01a59e7a28fbb616d2a7ef6fb7c7
USER <USER@DESKTOP-JBEE05O>
2024-07-13 20:50:47 +0530

    Initial commit

---
+++ JavPro/src/mypack/TestOrang.java
@@ -0,0 +1,34 @@
+package mypack;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+public class TestOrang {
+	public static void main(String[] args)
+	{
+	WebDriver driver = new ChromeDriver();
+	try
+	{
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	
+	WebElement usernameField = driver.findElement(By.name("username"));
+    usernameField.sendKeys("Admin");
+
+    // Find the password field using its name attribute and enter the password
+    WebElement passwordField = driver.findElement(By.name("password"));
+    passwordField.sendKeys("admin123");
+
+    // Find the login button using its ID or another attribute and click it
+    WebElement loginButton = driver.findElement(By.id("loginButton"));
+    loginButton.click();
+	}
+	  catch (Exception e) 
+	{
+         e.printStackTrace();
+     } 
+	finally {
+         // Close the browser
+         driver.quit();
+	}
+	}
+}
